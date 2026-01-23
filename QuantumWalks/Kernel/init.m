7f98f1c6d84cb067243dd4c5a8852d5879a05a3b
JA de Leon <deleongarrido.jose@gmail.com>
2026-01-23 17:41:39 -0600

    Added new geometries to load

--- QuantumWalks/Kernel/init.m
+++ QuantumWalks/Kernel/init.m
@@ -4,26 +4,20 @@
    Se ejecuta autom\[AAcute]ticamente al cargar << QuantumWalks` 
 *)
 
-Block[{$ContextPath},
-    Module[{rootPath},
-        (* 1. Obtener la ruta ra\[IAcute]z del paquete *)
-        (* Kernel/init.m -> Subir dos niveles -> Ra\[IAcute]z del paquete *)
-        rootPath = DirectoryName[DirectoryName[$InputFileName]];
-        
-        (* 2. Cargar el m\[OAcute]dulo base (1D / General) *)
-        Get[FileNameJoin[{rootPath, "DQWL.wl"}]];
-
-        (* 3. Cargar librer\[IAcute]as de Billares (Respetando CamelCase) *)
-        
-        (* A. Cargar Common.wl primero (l\[OAcute]gica base) *)
-        (* Nota: Carpeta "Billiards" y archivo "Common.wl" *)
-        Get[FileNameJoin[{rootPath, "Billiards", "Common.wl"}]];
-        
-        (* B. Cargar Bunimovich.wl (depende de Common) *)
-        (* Nota: Carpeta "Billiards" y archivo "Bunimovich.wl" *)
-        Get[FileNameJoin[{rootPath, "Billiards", "Bunimovich.wl"}]];
-        
-        (* Futuros billares: *)
-        (* Get[FileNameJoin[{rootPath, "Billiards", "Rectangle.wl"}]]; *)
-    ]
+(* QuantumWalks/Kernel/init.m *)
+Module[{rootPath},
+    rootPath = DirectoryName[DirectoryName[$InputFileName]];
+    
+    (* 1. Cargar Core *)
+    Get[FileNameJoin[{rootPath, "DQWL.wl"}]];
+    
+    (* 2. Cargar Utilidades *)
+    Get[FileNameJoin[{rootPath, "Billiards", "Common.wl"}]];
+    
+    (* 3. Cargar Geometr\[IAcute]as *)
+    Get[FileNameJoin[{rootPath, "Billiards", "Bunimovich.wl"}]];
+    Get[FileNameJoin[{rootPath, "Billiards", "Sinai.wl"}]];
+    Get[FileNameJoin[{rootPath, "Billiards", "Cardioid.wl"}]];
+    Get[FileNameJoin[{rootPath, "Billiards", "Rectangle.wl"}]];
+    Get[FileNameJoin[{rootPath, "Billiards", "Ellipse.wl"}]];
 ];
